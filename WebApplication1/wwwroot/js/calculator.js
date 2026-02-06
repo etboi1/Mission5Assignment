31aae6dd5f43cbdf0374817a032870fafcbd54e9
etboi1 <et.grundvig@gmail.com>
2026-02-06 14:12:50 -0700

    Added comments to the jQuery

--- WebApplication1/wwwroot/js/calculator.js
+++ WebApplication1/wwwroot/js/calculator.js
@@ -1,4 +1,9 @@
-﻿$(document).ready(function () {
+﻿// jQuery which
+//      a) waits until the page is loaded
+//      b) waits until the calc-btn is clicked
+//      c) verifies that the hours were a positive number and stops the calculation if it's not
+//      d) Outputs the calculated value
+$(document).ready(function () {
     $("#calc-btn").on("click", function () {
         if (parseInt($("#hour-input").val()) < 0) {
             alert("Number of tutoring hours must be positive.");
