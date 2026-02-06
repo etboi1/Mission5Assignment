a0410a7e2f818f747abe7caa553a13995713237c
etboi1 <et.grundvig@gmail.com>
2026-02-06 13:58:26 -0700

    Made the jQuery logic work and debugged it

--- WebApplication1/wwwroot/js/calculator.js
+++ WebApplication1/wwwroot/js/calculator.js
@@ -1,1 +1,12 @@
-﻿
+﻿$(document).ready(function () {
+    $("#calc-btn").on("click", function () {
+        if (parseInt($("#hour-input").val()) < 0) {
+            alert("Number of tutoring hours must be positive.");
+        } else {
+            let hours = parseInt($("#hour-input").val());
+            let hourlyRate = parseInt($("#hourly-rate").val());
+
+            $("#output-box").val(`$${hours * hourlyRate}`);
+        }
+    });
+});
